26f581936ac700daf0d32cabe14d24dde1d057c7
avez07 <ansariaavez12345@gmail.com>
2024-01-12 22:00:20 +0530

    t

--- src/app/common/admin-sidebar.js
+++ src/app/common/admin-sidebar.js
@@ -12,18 +12,19 @@ import { FaGear, FaTruckMoving } from "react-icons/fa6";
 const playball = Playball({ weight:'400', style: 'normal',subsets: ['latin'],display: 'swap',})
 
 const Sidebar = ({ isOpen, isNightMode }) => {
-  const [active,setActive] = useState(2);
+  const [active,setActive] = useState(0);
   const [subActive,setSubActive] = useState(1);
 
-  const handleactive = (value) => {
-    setActive((prevActive) => (prevActive === value ? prevActive : value));
-    setSubActive(1);
-  };
+  
 
   const handleSubactive = (value) => {
     setSubActive((prevSubActive) => (prevSubActive === value ? prevSubActive : value));
+
   };
-console.log(active)
+  const handleactive = (value) => {
+    setActive((prevActive) => (prevActive === value ? prevActive : value));
+    setSubActive(1)    
+  };
 
   return (
     <>
@@ -33,47 +34,47 @@ console.log(active)
             <Image src={logo} alt="website-logo" className="website-logo" /><span className={`${playball.className} website-name`}>Sweet delight</span>
           </div>
           <ul className="sidebar-ul">
-            <li className={`${active == 1? 'active': ''}`} onClick={()=>handleactive(1)}>
-              <Link href="/admin"><span className='menu-items'><FaHome /></span>Dashboard</Link>
+            <li className={`${active == 1? 'active': ''}`}>
+              <Link href="/admin" onClick={()=>handleactive(1)}><span className='menu-items'><FaHome /></span>Dashboard</Link>
             </li>
-            <li className={`${active == 2? 'active': ''}`} onClick={()=>handleactive(2)}>
-              <Link href="/admin/Inventory" ><span className="menu-items"><FaBoxOpen /></span>Product<span className="ms-auto right-arrow"><FaAngleRight/></span></Link>
+            <li className={`${active == 2? 'active': ''}`}>
+              <Link href="/admin/Inventory" onClick={()=>handleactive(2)} ><span className="menu-items"><FaBoxOpen /></span>Product<span className="ms-auto right-arrow"><FaAngleRight/></span></Link>
               <ul className="list-dropdwon p-0">
                 <li className={`${subActive == 1? 'sub-active': ''}`} onClick={()=>handleSubactive(1)}>List</li>
                 <li className={`${subActive == 2? 'sub-active': ''}`} onClick={()=>handleSubactive(2)}>Add</li>
                 <li className={`${subActive == 3? 'sub-active': ''}`} onClick={()=>handleSubactive(3)}>Category</li>
               </ul>
             </li>
-            <li className={`${active == 3? 'active': ''}`} onClick={()=>handleactive(3)}>
-              <Link href="/admin/Inventory" ><span className="menu-items"><FaCartPlus /></span>Orders<span className="ms-auto right-arrow"><FaAngleRight/></span></Link>
+            <li className={`${active == 3? 'active': ''}`} >
+              <Link href="/admin/Inventory" onClick={()=>handleactive(3)} ><span className="menu-items"><FaCartPlus /></span>Orders<span className="ms-auto right-arrow"><FaAngleRight/></span></Link>
               <ul className="list-dropdwon p-0">
                 <li className={`${subActive == 1? 'sub-active': ''}`} onClick={()=>handleSubactive(1)}>List</li>
                 <li className={`${subActive == 2? 'sub-active': ''}`} onClick={()=>handleSubactive(2)}>Details</li>
               </ul>
             </li>
-            <li className={`${active == 4? 'active': ''}`} onClick={()=>handleactive(4)}>
-              <Link href="/admin/Inventory" ><span className="menu-items"><FaUserFriends /></span>Seller<span className="ms-auto right-arrow"><FaAngleRight/></span></Link>
+            <li className={`${active == 4? 'active': ''}`} >
+              <Link href="/admin/Inventory" onClick={()=>handleactive(4)}><span className="menu-items"><FaUserFriends /></span>Seller<span className="ms-auto right-arrow"><FaAngleRight/></span></Link>
               <ul className="list-dropdwon p-0">
                 <li className={`${subActive == 1? 'sub-active': ''}`} onClick={()=>handleSubactive(1)}>List</li>
                 <li className={`${subActive == 2? 'sub-active': ''}`} onClick={()=>handleSubactive(2)}>Details</li>
               </ul>
             </li>
-            <li className={`${active == 5? 'active': ''}`} onClick={()=>handleactive(5)}>
-              <Link href="/"><span className="menu-items"><FaStar /></span>Reviews</Link>
+            <li className={`${active == 5? 'active': ''}`} >
+              <Link href="/" onClick={()=>handleactive(5)}><span className="menu-items"><FaStar /></span>Reviews</Link>
             </li>
-            <li className={`${active == 6? 'active': ''}`} onClick={()=>handleactive(6)}>
-              <Link href="/admin/Inventory" ><span className="menu-items"><FaTruckMoving /></span>Logistic<span className="ms-auto right-arrow"><FaAngleRight/></span></Link>
+            <li className={`${active == 6? 'active': ''}`} >
+              <Link href="/admin/Inventory" onClick={()=>handleactive(6)}><span className="menu-items"><FaTruckMoving /></span>Logistic<span className="ms-auto right-arrow"><FaAngleRight/></span></Link>
               <ul className="list-dropdwon p-0">
                 <li className={`${subActive == 1? 'sub-active': ''}`} onClick={()=>handleSubactive(1)}>Carriers List</li>
                 <li className={`${subActive == 2? 'sub-active': ''}`} onClick={()=>handleSubactive(2)}>Add</li>
                 <li className={`${subActive == 3? 'sub-active': ''}`} onClick={()=>handleSubactive(3)}>Category</li>
               </ul>
             </li>
-            <li className={`${active == 7? 'active': ''}`} onClick={()=>handleactive(7)}>
-              <Link href="/"><span className="menu-items"><FaGear /></span>settings</Link>
+            <li className={`${active == 7? 'active': ''}`} >
+              <Link href="/" onClick={()=>handleactive(7)}><span className="menu-items"><FaGear /></span>settings</Link>
             </li>
-            <li className={`${active == 8? 'active': ''}`} onClick={()=>handleactive(8)}>
-              <Link href="/"><span className="menu-items"><RiLogoutBoxFill /></span>Logout</Link>
+            <li className={`${active == 8? 'active': ''}`} >
+              <Link href="/" onClick={()=>handleactive(8)}><span className="menu-items"><RiLogoutBoxFill /></span>Logout</Link>
             </li>
           </ul>
         </div>
