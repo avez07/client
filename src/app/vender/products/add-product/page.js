0b698d4950aed29e1c3eb919aa32aed784cda72e
avez07 <ansariaavez12345@gmail.com>
2024-02-09 18:21:52 +0530

    materail ui textfeild added

--- src/app/vender/products/add-product/page.js
+++ src/app/vender/products/add-product/page.js
@@ -1,16 +1,17 @@
 "use client"
 import React, { useEffect, useState, useContext } from "react";
-import {useFormik} from 'formik'
+import { useFormik, Field } from 'formik'
 import * as yup from 'yup'
 import { AuthContext } from '@/app/common/auth'
 import dynamic from 'next/dynamic';
-import { RadioGroup, FormControlLabel, Radio, Checkbox } from '@mui/material'
+import { RadioGroup, FormControlLabel, TextField, Radio, Checkbox } from '@mui/material'
 import { Form, Button, InputGroup } from "react-bootstrap";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import Tooltip from "react-bootstrap/Tooltip";
 import { FaRegQuestionCircle } from "react-icons/fa";
 import 'react-quill/dist/quill.snow.css';
 import BulkEdiTable from "@/app/common/table-bulkEdit";
+import { Label } from "@mui/icons-material";
 const Quill = dynamic(() => import('react-quill'), { ssr: false })
 const Select = dynamic(() => import('react-select'), { ssr: false })
 
@@ -35,14 +36,13 @@ const main_size = () => {
 
 const BulkEdit = () => {
   const { nightmode } = useContext(AuthContext);
-  const [description, setDescription] = useState("");
   const [sell, setSell] = useState("");
   const [cost, setCost] = useState("");
   const [profit, setProfit] = useState("");
-  const [margin, setMargin] = useState(0);
+  const [margin, setMargin] = useState("");
   const [gst, setGst] = useState(0);
   const [Campare, setCampare] = useState("");
-  const [discount, setDiscount] = useState("00");
+  const [discount, setDiscount] = useState("");
   const [weight, setWeight] = useState("");
   const [shipCharge, setShipCharge] = useState(0);
   const [VariantTure, setVariantTure] = useState(false);
@@ -54,7 +54,6 @@ const BulkEdit = () => {
   const calculateMargin = () => {
     const calculatedProfit = sell - cost;
     const calculatedMargin = (calculatedProfit / sell) * 100;
-    // const calculateGst = sell < '1000'? 5 : 12;
     const calculateGst = 18;
     const calculateDiscount = ((Campare - sell) / Campare) * 100;
     const finalDiscount = calculateDiscount < 0 ? 0 : calculateDiscount;
@@ -67,24 +66,24 @@ const BulkEdit = () => {
     setShipCharge(ShipingCharges.toFixed(2));
   };
   const customStyle = {
-    control: (style)=>({...style,background:nightmode?'#0c1220':null,border:'currentColor'}),
-    singleValue: (style)=>({...style,color: nightmode?'#fff':null}),
-    menu: (style)=>({...style,background:nightmode?'#0c1220':null}),
-    option: (styles, { data, isDisabled, isFocused, isSelected  }) => {
+    control: (style) => ({ ...style, background: nightmode ? '#0c1220' : null, border: 'currentColor' }),
+    singleValue: (style) => ({ ...style, color: nightmode ? '#fff' : null }),
+    menu: (style) => ({ ...style, background: nightmode ? '#0c1220' : null }),
+    option: (styles, { data, isDisabled, isFocused, isSelected }) => {
       // const color = chroma(data.color);
       return {
         ...styles,
         backgroundColor: isFocused ? "#ff0000" : null,
-        background : nightmode ?'#0c1220' : '#fff',
-        color: nightmode? '#fcfcfc':"#333333",
-        ':active':{
+        background: nightmode ? '#0c1220' : '#fff',
+        color: nightmode ? '#fcfcfc' : "#333333",
+        ':active': {
           ...styles['.active'],
-          background:'#232836'
+          background: '#232836'
         },
-        ':hover':{
+        ':hover': {
           ...styles['.hover'],
-          backgroundColor:'#fcfcfc',
-          color:'#000'
+          backgroundColor: '#fcfcfc',
+          color: '#000'
         }
       };
     }
@@ -115,7 +114,7 @@ const BulkEdit = () => {
   });
 
   // console.log(ColorList)
- 
+
   const handleVariant = (e) => {
     const storedVariantData = JSON.parse(localStorage.getItem('variantData'));
     // console.log(storedVariantData.length); return false
@@ -132,11 +131,11 @@ const BulkEdit = () => {
     if (Array.isArray(storedVariantData)) {
       if (storedVariantData.some(variant => variant.color == VariantColor.value && variant.size == VariantSize.value)) {
         alert('variant ALready Present')
-      }else if(!VariantColor.value || !VariantSize.value){
+      } else if (!VariantColor.value || !VariantSize.value) {
         alert('variant Value Not Present')
-      }else{
+      } else {
         const updatedVariantObject = storedVariantData ? [...storedVariantData, newVariant] : [newVariant];
-        localStorage.setItem('variantData', JSON.stringify(updatedVariantObject));        
+        localStorage.setItem('variantData', JSON.stringify(updatedVariantObject));
       }
     } else {
       localStorage.setItem('variantData', JSON.stringify([newVariant]));
@@ -154,26 +153,30 @@ const BulkEdit = () => {
   const ProductValidationSchema = yup.object({
     productName: yup.string().required('This Feild is Required'),
     ProductCategory: yup.string().required('This Feild is Required'),
-    ProductDiscription : yup.string().required('This Feild is Required').max(200,'Discription Should Not Be More Than 200 Characters')
+    ProductDiscription: yup.string().required('This Feild is Required').max(200, 'Discription Should Not Be More Than 200 Characters')
 
   })
-const Formik = useFormik({
-  initialValues:{
-    productName : '',
-    ProductCategory: '',
-    ProductDiscription:'',
-    InitailPrice : '',
-    InitailCost : '',
-    CamparePrice : '',
-    Weight : ''
+  const Formik = useFormik({
+    initialValues: {
+      productName: '',
+      ProductCategory: '',
+      ProductDiscription: '',
+      InitailPrice: '',
+      InitailCost: '',
+      CamparePrice: '',
+      Weight: ''
 
-  },validationSchema:ProductValidationSchema,
-onSubmit:(values)=>{
-console.log('form submited',values)
-}
+    }, validationSchema: ProductValidationSchema,
+    onSubmit: (values) => {
+      console.log('form submited', values)
+    }
 
-})
-  
+  })
+  const handleQuillChange = (content) => {
+    Formik.setFieldValue('ProductDiscription', content); // Update Formik's state
+  };
+
+
   return (
     <>
       <Form onSubmit={Formik.handleSubmit}>
@@ -188,134 +191,77 @@ console.log('form submited',values)
             onBlur={Formik.handleBlur}
             placeholder="Enter name"
           />
-          {Formik.touched.productName && Formik.errors.productName? (
-                  <div className="text-danger">{Formik.errors.productName}</div>
-                ) : (null)}
+          {Formik.touched.productName && Formik.errors.productName ? (
+            <div className="text-danger">{Formik.errors.productName}</div>
+          ) : (null)}
         </Form.Group>
         <Form.Group controlId="formName" className="card my-3 p-3">
           <Form.Label className="fw-semibold">Category</Form.Label>
           <Select
-                  name="ProductCategory"
-                  menuPlacement="top"
-                  className="categoryName"
-                  styles={{...customStyle,width:'100%' }}                  
-                  onChange={(e)=>Formik.setFieldValue('ProductCategory',e.value)}
-                  onBlur={Formik.handleBlur}
-                  options={[
-                    { value: 'Kidwear', label: 'Kidwear'},
-                    { value: 'bridal wear', label: 'bridal wear'},
-                    { value: 'women', label: 'women'},
-                    { value: 'Men', label: 'Men'}
-                  ]}
-                />
-                 {Formik.touched.ProductCategory && Formik.errors.ProductCategory? (
-                  <div className="text-danger">{Formik.errors.ProductCategory}</div>
-                ) : (null)}
+            name="ProductCategory"
+            menuPlacement="top"
+            className="categoryName"
+            styles={{ ...customStyle, width: '100%' }}
+            onChange={(e) => Formik.setFieldValue('ProductCategory', e.value)}
+            onBlur={Formik.handleBlur}
+            options={[
+              { value: 'Kidwear', label: 'Kidwear' },
+              { value: 'bridal wear', label: 'bridal wear' },
+              { value: 'women', label: 'women' },
+              { value: 'Men', label: 'Men' }
+            ]}
+          />
+          {Formik.touched.ProductCategory && Formik.errors.ProductCategory ? (
+            <div className="text-danger">{Formik.errors.ProductCategory}</div>
+          ) : (null)}
         </Form.Group>
 
         <Form.Group controlId="formDescription" className="card my-3 p-3">
-          <Form.Label  className="fw-semibold">Description</Form.Label>
+          <Form.Label className="fw-semibold">Description</Form.Label>
+
           <Quill theme="snow"
-            id="Discription"           
+            id="Discription"
             modules={{
               toolbar: toolbarOptions
             }}
-            // name='ProductDiscription'
             value={Formik.values.ProductDiscription}
-            onChange={Formik.handleChange}
+            onChange={handleQuillChange}
             onBlur={Formik.handleBlur}
           />
-           {Formik.touched.ProductDiscription && Formik.errors.ProductDiscription? (
-                  <div className="text-danger">{Formik.errors.ProductDiscription}</div>
-                ) : (null)}
+
+
+          {Formik.touched.ProductDiscription && Formik.errors.ProductDiscription ? (
+            <div className="text-danger">{Formik.errors.ProductDiscription}</div>
+          ) : (null)}
         </Form.Group>
         <Form.Group controlId="formName" className="card my-3 p-3">
           <Form.Label className="fw-semibold me-3">Pricing</Form.Label>
-          <div className="d-flex align-items-center my-3">
-            <InputGroup className="me-3" style={{ width: "30%" }}>
-              <InputGroup.Text className="inputgroup-text">
-                Cost
-              </InputGroup.Text>
-              <Form.Control
-                className="add-name"
-                type="text"
-                name="cost"
-                onChange={(e) => setCost(e.target.value)}
-                placeholder="0.00"
-              />
-            </InputGroup>
-            <InputGroup className="me-3" style={{ width: "30%" }}>
-              <InputGroup.Text className="inputgroup-text">
-                Price
-              </InputGroup.Text>
-              <Form.Control
-                className="add-name"
-                type="text"
-                name="sell"
-                onChange={(e) => setSell(e.target.value)}
-                placeholder="0.00"
-              />
-            </InputGroup>
-            <InputGroup style={{ width: "30%" }}>
-              <InputGroup.Text className="inputgroup-text">
-                Campare Price
-              </InputGroup.Text>
-              <Form.Control
-                className="add-name"
-                type="text"
-                name="campare"
-                onChange={(e) => setCampare(e.target.value)}
-                placeholder="0.00"
-              />
-            </InputGroup>
-          </div>
-          <div className="d-flex align-items-center">
-            <InputGroup className="me-3" style={{ width: "25%" }}>
-              <InputGroup.Text className="inputgroup-text">
-                Discount
-              </InputGroup.Text>
-              <Form.Control
-                className="add-name"
-                type="text"
-                disabled
-                value={`${discount}%`}
-                placeholder="0.00"
-              />
-            </InputGroup>
-            <InputGroup className="me-3" style={{ width: "25%" }}>
-              <InputGroup.Text className="inputgroup-text">
-                Profit
-              </InputGroup.Text>
-              <Form.Control
-                className="add-name"
-                type="text"
-                disabled
-                value={profit}
-                placeholder="0.00"
-              />
-            </InputGroup>
-            <InputGroup className="me-3" style={{ width: "25%" }}>
-              <InputGroup.Text className="inputgroup-text">
-                Margin
-              </InputGroup.Text>
-              <Form.Control
-                className="add-name"
-                type="text"
-                disabled
-                value={`${margin}%`}
-                placeholder="0.00"
-              />
-            </InputGroup>
-            <InputGroup style={{ width: "25%" }}>
-              <InputGroup.Text className="inputgroup-text">Gst</InputGroup.Text>
-              <Form.Control
-                className="add-name"
-                type="text"
-                disabled
-                value={`${gst}%`}
-                placeholder="0.00"
-              />
-            </InputGroup>
+          <div className=" pricingInputContainer">
+            <div className="price-inputContainer">
+              <label className="fw-normal">Price</label>
+              <TextField id="outlined-multiline-flexible" onChange={(e)=>setSell(e.target.value)}  className="add-name" size="small" variant="outlined" />
+            </div>
+            <div className="price-inputContainer">
+              <label className="fw-normal">Cost</label>
+              <TextField id="outlined-multiline-flexible" onChange={(e)=>setCost(e.target.value)} className="add-name" size="small" variant="outlined" />
+            </div>
+            <div className="price-inputContainer">
+              <label className="fw-normal">Campare Price</label>
+              <TextField id="outlined-multiline-flexible" onChange={(e)=>setCampare(e.target.value)} className="add-name"  size="small" variant="outlined" />
+            </div>
+            <div className="price-inputContainer">
+              <label className="fw-normal">Profit</label>
+              <TextField  className="add-name"  value={profit || '-- --'}  id="outlined-multiline-flexible" size="small" variant="outlined" />
+            </div>
+            <div className="price-inputContainer">
+              <label className="fw-normal">Margin</label>
+              <TextField  className="add-name" value={margin}  id="outlined-multiline-flexible" size="small" variant="outlined" />
+            </div>
+            <div className="price-inputContainer">
+              <label className="fw-normal">Gst</label>
+              <TextField  className="add-name" value={gst || '-- --'}  id="outlined-multiline-flexible" size="small" variant="outlined" />
+            </div>
+
           </div>
         </Form.Group>
         <Form.Group controlId="formDescription" className="card my-3 p-3">
@@ -372,8 +318,8 @@ console.log('form submited',values)
                   value={variantSelect}
                 >
                   <FormControlLabel value='1' label='Color Variant' control={<Radio size="small" sx={{ '&.Mui-checked': { color: nightmode ? '#fff' : '#000' } }} />} />
-                  <FormControlLabel value='2' label='Size Variant' control={<Radio size="small" sx={{ '&.Mui-checked': { color: nightmode ? '#fff' : '#000' } }}/>} />
-                  <FormControlLabel value='3' label='Both' control={<Radio size="small" sx={{ '&.Mui-checked': { color: nightmode ? '#fff' : '#000' } }}/>} />
+                  <FormControlLabel value='2' label='Size Variant' control={<Radio size="small" sx={{ '&.Mui-checked': { color: nightmode ? '#fff' : '#000' } }} />} />
+                  <FormControlLabel value='3' label='Both' control={<Radio size="small" sx={{ '&.Mui-checked': { color: nightmode ? '#fff' : '#000' } }} />} />
 
                 </RadioGroup>
               </div>
@@ -390,8 +336,8 @@ console.log('form submited',values)
                 <Select
                   value={VariantSize}
                   menuPlacement="top"
-                  styles={{...customStyle,backgroundColor:'red', width: '10vw' }}
-                  
+                  styles={{ ...customStyle, backgroundColor: 'red', width: '10vw' }}
+
                   onChange={(selectedOption) => setVariantSize(selectedOption)}
                   options={main_size()}
                 />
