5ca4b63918c06bc22a0fd84ab041aac26765aa1c
avez07 <ansariaavez12345@gmail.com>
2024-03-22 16:24:28 +0530

    redevelopement

--- src/app/dashboard/single-view/page.js
+++ src/app/dashboard/single-view/page.js
@@ -144,7 +144,7 @@ const SinglePage = () => {
                     </div>
                     <div className="fw-bold mt-3 mb-2 fs-5">Discription: </div>
                     <div>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                     </div>
                 </Col>
             </Row>
@@ -164,14 +164,12 @@ const SinglePage = () => {
                                                 </span>
                                             </div>
                                             <div className="review body">
-                                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since
+                                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since
                                             </div>
                                         </Card.Body>
                                     </Card>
                                 </div>
                             ))}
-
-
                         </Slider>
                     </div>
                 </Col>
