7f8faad0f36b8f4a77c61c56b5be0954f07fffc3
avez07 <ansariaavez12345@gmail.com>
2024-04-16 15:14:46 +0530

    r

---
+++ src/app/loading.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import {Skeleton,Stack,Grid} from '@mui/material/';
+
+export default function Variants() {
+  return (
+    <>
+    <Stack spacing={3}>
+      {/* For variant="text", adjust the height via font-size */}
+      <Skeleton variant="rectangular" width="100%" height={60} animation='wave' />
+      <Skeleton variant="rectangular" width="100%" height={200} />
+    </Stack>
+     <Stack spacing={3}>
+     {/* For variant="text", adjust the height via font-size */}
+     <Skeleton variant="rectangular" width="100%" height={200} />
+   </Stack>
+   <div className='d-flex mt-3 justify-content-between'>
+   <Skeleton variant="rectangular" width='48%' height={158} />
+   <Skeleton variant="rectangular" width='48%' height={158} />
+   </div>
+   </>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,49 @@
+"use client"
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import Cookies from 'js-cookie';
 
-import {redirect} from "next/navigation";
+const YourComponent = () => {
+  const [loginData, setLoginData] = useState(null);
+  const [apiError, setApiError] = useState(null);
+  const router = useRouter();
 
-const redirect_page = ()=>{
-  redirect('/admin')
-}
-export default redirect_page;
+  useEffect(() => {
+    const fetchData = async () => {
+
+      try {
+        const response = await fetch(process.env.NEXT_PUBLIC_APP_URL + 'authToken', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'authorization': 'Bearer ' + Cookies.get('token')
+          }
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data)
+          setLoginData(data);
+        } else {
+          const error = await response.json();
+          setApiError(error.message);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+
+    };
+
+
+
+    // Redirect logic should be inside useEffect after fetchData
+    if (!loginData) { fetchData(); }
+    else {
+      const redirectPath = loginData.role === 'user' ? '/dashboard' : loginData.role === 'vender' ? '/vender' : loginData.role === 'admin' ? '/admin' : '/authentication/login';
+      router.push(redirectPath);
+    }
+
+  }, [loginData]); // Depend on loginData only
+};
+
+export default YourComponent;
