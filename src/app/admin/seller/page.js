44da3ad03b1f8e99734709485e56a0fe019b87df
avez07 <ansariaavez12345@gmail.com>
2024-04-26 17:58:06 +0530

    this is commit

--- src/app/admin/seller/page.js
+++ src/app/admin/seller/page.js
@@ -2,7 +2,8 @@
 import React, { useEffect, useState } from 'react';
 import { MaterialReactTable, createMRTColumnHelper, useMaterialReactTable, } from 'material-react-table';
 import { mkConfig, generateCsv, download } from 'export-to-csv';
-import { Box, Button } from '@mui/material';
+import { Box, Button, MenuItem, ListItemIcon } from '@mui/material';
+import { AccountCircle, Send } from '@mui/icons-material';
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
 import { jsPDF } from 'jspdf'; //or use your library of choice here
 import autoTable from 'jspdf-autotable';
@@ -24,32 +25,24 @@ const handleExportRows = (rows) => {
 const Seller = () => {
     const [data, setData] = useState([])
     const [active, setActive] = useState(true)
-    const [actions, setActions] = useState(false)
+
+    useEffect(() => {
+        if (active) {
+            fetchSellerData()
+            setActive(false)
+        }
+    }, [data,active])
     const columns = useMemo(() => [
-        columnHelper.accessor('name', {
-            header: 'Name',
-            size: 40,
-        }),
-        columnHelper.accessor('email', {
-            header: 'Email',
-            size: 40,
-        }),
-        columnHelper.accessor('DOB', {
-            header: 'Date of Birth',
-            size: 120,
-
-        }),
-        columnHelper.accessor('createdAt', {
-            header: 'createdAt',
-            size: 120,
-
-        }),
+        columnHelper.accessor('name', { header: 'Name', size: 40, }),
+        columnHelper.accessor('email', { header: 'Email', size: 40, }),
+        columnHelper.accessor('DOB', { header: 'Date of Birth', size: 120, }),
+        columnHelper.accessor('createdAt', { header: 'createdAt', size: 120, }),
         columnHelper.accessor('active', {
             header: 'active',
             size: 120,
-            Cell: ({ cell }) => {
-                if (!cell.getValue()) return <FaThumbsDown className='fs-5 text-danger' />
-                return <FaThumbsUp className='fs-5 text-success' />
+            Cell: ({ cell,row }) => {
+                if (!cell.getValue()) return   <FaThumbsDown onClick={()=>handleSellerActive(row.original.loginId)} className='fs-5 text-danger' style={{ fill: 'red'  ,cursor:'pointer'}} />
+                return <FaThumbsUp  onClick={()=>handleSellerActive(row.original.loginId)} className='fs-5 text-success' style={{ fill: 'green',  cursor:'pointer'}} />
             }
 
         }),
@@ -57,30 +50,10 @@ const Seller = () => {
             header: 'suspended',
             size: 120,
             Cell: ({ cell }) => {
-                if (!cell.getValue()) return <FaThumbsDown className='fs-5 text-danger' />
-                return <FaThumbsUp className='fs-5 text-success' />
+                if (!cell.getValue()) return <FaThumbsDown  className='fs-5 text-danger' style={{ fill: 'red' , cursor:'pointer'}} />
+                return <FaThumbsUp  className='fs-5 text-success' style={{ fill: 'green', cursor:'pointer' }} />
             }
         }),
-        // columnHelper.display({
-        //     header: 'Actions',
-        //     size: 120,
-        //     Cell: ({ cell }) => {
-        //         return (
-        //             <Dropdown>
-        //                 <Dropdown.Toggle className='border-0' style={{ background: 'transparent' }}>
-        //                     <BsThreeDotsVertical className='text-center text-dark' style={{ cursor: 'pointer' }} />
-        //                 </Dropdown.Toggle>
-        //                 <Dropdown.Menu show={true}>
-        //                     <Dropdown.Item href="#">Menu Item</Dropdown.Item>
-        //                     <Dropdown.Item href="#">Menu Item</Dropdown.Item>
-        //                     <Dropdown.Item href="#">Menu Item</Dropdown.Item>
-        //                 </Dropdown.Menu>
-        //             </Dropdown>
-        //         )
-        //     }
-        // })
-
-
     ])
     const fetchSellerData = async () => {
         try {
@@ -93,6 +66,22 @@ const Seller = () => {
                 }
             })
             if (respose.ok) setData(await (await respose.json()).data)
+        } catch (error) {
+            console.log('erro While fetch the data', error)
+        }
+    }
+     const handleSellerActive = async (id) => {
+        try {
+            const token = Cookies.get('token')
+            const respose = await fetch(process.env.NEXT_PUBLIC_APP_URL + 'sellerActive', {
+                method: 'POST',
+                body : JSON.stringify({id:id}),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'authorization': 'Bearer ' + token
+                }
+            })
+            if (respose.ok) setActive(true)
         } catch (error) {
             console.log('erro While fetch the data', error)
         }
@@ -113,61 +102,40 @@ const Seller = () => {
         columns,
         data,
         enableRowSelection: true,
+        enableRowActions: true,
+        enableStickyHeader: true,
+        enableColumnPinning: true,
         enableColumnOrdering: false,
         enableColumnActions: false,
-        enableStickyHeader: true,
         enableHiding: false,
         columnFilterDisplayMode: 'popover',
         paginationDisplayMode: 'pages',
         positionToolbarAlertBanner: 'bottom',
-        renderRowActionMenuItems:({closemenu})=>{
-            <Button>button</Button>
+        positionActionsColumn: 'last',
+        muiPaginationProps: {
+            rowsPerPageOptions: [10, 25, 50, 1000]
         },
+        renderRowActionMenuItems: ({ closeMenu }) => [
+            <MenuItem key={0} onClick={() => { closeMenu(); }} sx={{ m: 0 }}>
+                <ListItemIcon><AccountCircle /></ListItemIcon>
+                View Profile
+            </MenuItem>,
+            <MenuItem key={1} onClick={() => { closeMenu(); }} sx={{ m: 0 }}>
+                <ListItemIcon><Send /></ListItemIcon>
+                Send Email
+            </MenuItem>,
+        ],
         renderTopToolbarCustomActions: ({ table }) => (
-            <Box
-                sx={{
-                    display: 'flex',
-                    gap: '16px',
-                    padding: '8px',
-                    flexWrap: 'wrap',
-                    color: 'red'
-                }}
-            >
-
-                <Button
-                    disabled={
-                        !table.getIsSomeRowsSelected() && !table.getIsAllRowsSelected()
-                    }
-                    //only export selected rows
-                    onClick={() => handleExportRowsPDF(table.getSelectedRowModel().rows)}
-                    startIcon={<FileDownloadIcon />}
-                >
-                    Export  PDF
-                </Button>
-                <Button
-                    disabled={
-                        !table.getIsSomeRowsSelected() && !table.getIsAllRowsSelected()
-                    }
-                    //only export selected rows
-                    onClick={() => handleExportRows(table.getSelectedRowModel().rows)}
-                    startIcon={<FileDownloadIcon />}
-                >
-                    Export EXCEL
-                </Button>
+            <Box sx={{ display: 'flex', gap: '16px', padding: '8px', flexWrap: 'wrap', color: 'red' }}>
+                <Button disabled={!table.getIsSomeRowsSelected() && !table.getIsAllRowsSelected()} onClick={() => handleExportRowsPDF(table.getSelectedRowModel().rows)} startIcon={<FileDownloadIcon />}>Export  PDF</Button>
+                <Button disabled={!table.getIsSomeRowsSelected() && !table.getIsAllRowsSelected()} onClick={() => handleExportRows(table.getSelectedRowModel().rows)} startIcon={<FileDownloadIcon />}>Export EXCEL</Button>
             </Box>
         ),
     });
-    useEffect(() => {
-        if (active) {
-
-            fetchSellerData()
-            setActive(false)
-        }
-    }, [data])
+   
 
     return (
         <>
-
             <div>
                 <MaterialReactTable table={table} />
             </div>
