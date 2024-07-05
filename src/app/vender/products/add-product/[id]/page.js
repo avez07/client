dc65f73d8bd5e0a6b4aa32c27cd0cab6b3843386
avez07 <ansariaavez12345@gmail.com>
2024-07-05 18:32:03 +0530

    data is not delete

--- src/app/vender/products/add-product/[id]/page.js
+++ src/app/vender/products/add-product/[id]/page.js
@@ -7,16 +7,21 @@ import { Button, Col, Form, Row } from "react-bootstrap";
 import { FaInfoCircle, FaPlus } from "react-icons/fa";
 import dynamic from "next/dynamic";
 import { IoClose } from "react-icons/io5";
+import { IoMdCloseCircleOutline } from "react-icons/io";
 const Select = dynamic(() => import('react-select'), { ssr: false })
 
 
+const ColorsOption = ["Black", "White", "Red", "Blue", "Green", "Yellow", "Pink", "Purple", "Orange", "Gray", "Brown", "Beige", "Navy", "Teal", "Turquoise", "Silver", "Gold", "Cream", "Burgundy", "Magenta", "Lavender", "Charcoal", "Olive", "Sky Blue", "Tan", "Maroon", "Indigo", "Emerald", "Sapphire", "Ruby", "Amber", "Ivory", "Coral", "Slate", "Pearl", "Champagne", "Peach", "Crimson", "Steel"];
 
 const AddInfo = ({ params }) => {
    const [CategoryData, setCategoryData] = useState({})
    const [pagecount, setPagecount] = useState(1)
    const [CategoryInput, setCategoryInput] = useState({})
+   const [variantOption, setVariantOption] = useState([]);
+   const [VariantTab2, setVariantTab2] = useState([])
+   const [VariantTab, setVariantTab] = useState({})
    const { nightmode } = useContext(AuthContext)
-   const [pointsCount,setPointsCount] = useState(1)
+   const [pointsCount, setPointsCount] = useState(1)
 
    const customStyle = {
       control: (style) => ({ ...style, background: nightmode ? '#0c1220' : null, border: nightmode ? 'currentColor' : '' }),
@@ -42,7 +47,38 @@ const AddInfo = ({ params }) => {
          };
       }
    }
+   console.log(VariantTab)
+   const handleVariantOptionChange = (e, index) => {
+      let updateVariant = [...variantOption]
+      const { name, checked } = e.target
+      if (checked) updateVariant = [...updateVariant, name]
+      if (!checked) updateVariant.splice(updateVariant.indexOf(name), 1)
+      setVariantOption(updateVariant)
+      setCategoryInput((prev) => ({
+         ...prev,
+         details: {
+            ...prev.details,
+            variantOption: updateVariant
+         }
+      }))
 
+   }
+   const handleVariantTab2 = (e)=>{
+     const data =  Object.values(VariantTab).join('/')
+     setVariantTab2((prev)=>([...prev,data]))
+   }
+   const handleDeleteVariantTab = (index)=>{
+      const data = VariantTab2.splice(index,1)
+      setVariantTab2(data)
+}
+   console.log(VariantTab2)
+   useEffect(() => {
+      setVariantTab(variantOption.reduce((acc, item) => {
+         acc[item] = '';
+         return acc;
+      }, {}));
+      setVariantTab2([])
+   }, [variantOption]);
    useEffect(() => {
       const id = params.id
       const token = Cookies.get('token')
@@ -52,7 +88,7 @@ const AddInfo = ({ params }) => {
          alert(response.message)
       })
    }, [])
-   console.log(CategoryInput)
+   // console.log(CategoryInput)
    return (
       <>
 
@@ -67,7 +103,7 @@ const AddInfo = ({ params }) => {
                </ul>
             </div>
          </Row>
-        {pagecount == 1&&( <div>
+         {pagecount == 1 && (<div>
             <Row md={1} className="g-3">
                {CategoryData && CategoryData.details ? (
                   CategoryData.details.virtualInfo.map((item, index) => {
@@ -75,18 +111,18 @@ const AddInfo = ({ params }) => {
                         return (
                            <Col key={`input-${index}`} md={parseInt(item.size)}>
                               <Form.Label className="fw-semibold">{item.name}:{item.Isimportant && (<span className="text-danger">*</span>)}</Form.Label>
-                              <Form.Control type="text" name={item.name}  onBlur={(e) =>
-                                    setCategoryInput((prev) => ({
-                                       ...prev,
-                                       details: {
-                                          ...prev.details,
-                                          virtualInfo: {
-                                             ...((prev.details && prev.details.virtualInfo) || {}),
-                                             [item.name]: e.target.value
-                                          }
+                              <Form.Control type="text" name={item.name} onBlur={(e) =>
+                                 setCategoryInput((prev) => ({
+                                    ...prev,
+                                    details: {
+                                       ...prev.details,
+                                       virtualInfo: {
+                                          ...((prev.details && prev.details.virtualInfo) || {}),
+                                          [item.name]: e.target.value
                                        }
-                                    }))
-                                 }/>
+                                    }
+                                 }))
+                              } />
                            </Col>
                         );
                      } else if (item.type === 'DropDwon') {
@@ -123,7 +159,7 @@ const AddInfo = ({ params }) => {
                ) : null}
             </Row>
          </div>)}
-        {pagecount == 5 &&( <div>
+         {pagecount == 5 && (<div>
             <Row md={1} className="g-3">
                {CategoryData && CategoryData.details ? (
                   CategoryData.details.MoreInfo.map((item, index) => {
@@ -131,18 +167,18 @@ const AddInfo = ({ params }) => {
                         return (
                            <Col key={`input-${index}`} md={parseInt(item.size)}>
                               <Form.Label className="fw-semibold">{item.name}:{item.Isimportant && (<span className="text-danger">*</span>)}</Form.Label>
-                              <Form.Control type="text" name={item.name}  onBlur={(e) =>
-                                    setCategoryInput((prev) => ({
-                                       ...prev,
-                                       details: {
-                                          ...prev.details,
-                                          MoreInfo: {
-                                             ...((prev.details && prev.details.MoreInfo) || {}),
-                                             [item.name]: e.target.value
-                                          }
+                              <Form.Control type="text" name={item.name} onBlur={(e) =>
+                                 setCategoryInput((prev) => ({
+                                    ...prev,
+                                    details: {
+                                       ...prev.details,
+                                       MoreInfo: {
+                                          ...((prev.details && prev.details.MoreInfo) || {}),
+                                          [item.name]: e.target.value
                                        }
-                                    }))
-                                 }/>
+                                    }
+                                 }))
+                              } />
                            </Col>
                         );
                      } else if (item.type === 'DropDwon') {
@@ -179,46 +215,103 @@ const AddInfo = ({ params }) => {
                ) : null}
             </Row>
          </div>)}
-         {pagecount == 2 &&(
+         {pagecount == 2 && (
             <Row className="g-3">
                <Col md={12}>
                   <Form.Label className="fw-semibold mx-3">Discription</Form.Label>
-                  <Form.Control as='textarea' rows={10} style={{resize:'none'}} onBlur={(e) =>
-                                    setCategoryInput((prev) => ({
-                                       ...prev,
-                                       details: {
-                                          ...prev.details,
-                                         ['discription'] : e.target.value
-                                       }
-                                    }))
-                                 }/>
+                  <Form.Control as='textarea' rows={10} style={{ resize: 'none' }} onBlur={(e) =>
+                     setCategoryInput((prev) => ({
+                        ...prev,
+                        details: {
+                           ...prev.details,
+                           ['discription']: e.target.value
+                        }
+                     }))
+                  } />
                </Col>
                <Col md={12}>
-               <div className="d-flex justify-content-between mx-3">
-                  <Form.Label className="fw-semibold">Add Bulet Points</Form.Label>
-                  <div>
-                     <span className="me-2 fw-semibold">Max Points {pointsCount}/6</span>
-                     {pointsCount < 6 && (<Button onClick={(e)=>{setPointsCount((prev)=>prev+1),window.scrollTo({top:document.body.scrollHeight,behavior:'instant'})}} type="button" style={{border:'none',background:'#362465'}}><FaPlus/> ADD</Button>)}
+                  <div className="d-flex justify-content-between mx-3">
+                     <Form.Label className="fw-semibold">Add Bulet Points</Form.Label>
+                     <div>
+                        <span className="me-2 fw-semibold">Max Points {pointsCount}/6</span>
+                        {pointsCount < 6 && (<Button onClick={(e) => { setPointsCount((prev) => prev + 1), window.scrollTo({ top: document.body.scrollHeight, behavior: 'instant' }) }} type="button" style={{ border: 'none', background: '#362465' }}><FaPlus /> ADD</Button>)}
+                     </div>
                   </div>
-               </div>
-               <ul>
-               {Array.from({length : pointsCount},(_,index)=>{ console.log(index); return(
-                     <li key={index} className="d-flex align-items-center"><Form.Control size="sm" onBlur={(e) =>
-                        setCategoryInput((prev) => ({
-                           ...prev,
-                           details: {
-                             ...prev.details,
-                             bulletPoints: [
-                              ...prev.details?.bulletPoints || [],
-                              [index] = `index_${index}` 
-                             ]
-                           }
-                        }))
-                     } className="my-3 me-auto" style={{width:'95%'}} name="buletPoints" type="text"/><span className="fs-4 fw-bold mx-auto text-danger" style={{cursor:'pointer'}} onClick={(e)=>setPointsCount((prev)=>prev - 1)}><IoClose/></span></li>
-                  )})}
+                  <ul>
+                     {Array.from({ length: pointsCount }, (_, index) => {
+                        console.log(index); return (
+                           <li key={index} className="d-flex align-items-center"><Form.Control size="sm" onBlur={(e) =>
+                              setCategoryInput((prev) => ({
+                                 ...prev,
+                                 details: {
+                                    ...prev.details,
+                                    bulletPoints: [
+                                       ...prev.details?.bulletPoints || [],
+                                       [index] = `index_${index}`
+                                    ]
+                                 }
+                              }))
+                           } className="my-3 me-auto" style={{ width: '95%' }} name="buletPoints" type="text" /><span className="fs-4 fw-bold mx-auto text-danger" style={{ cursor: 'pointer' }} onClick={(e) => setPointsCount((prev) => prev - 1)}><IoClose /></span></li>
+                        )
+                     })}
                   </ul>
                </Col>
             </Row>
+         )}
+         {pagecount == 3 && (
+            <div>
+               <Row className="g-3">
+                  <Col md={12}>
+                     <div className="d-flex justify-content-around my-4"><span className="fw-semibold">Variant Containt:</span><span className="d-flex justify-content-around w-50">{CategoryData.details.VariantOption.map((items, index) => (
+                        <span><Form.Check type="checkbox" name={items} onChange={(e) => handleVariantOptionChange(e, index)} key={index} label={items} /></span>))}</span></div>
+                  </Col>
+                  {variantOption.length > 0 && (
+                     <>
+                        <Col md={4} className="my-5">
+                           <div className="">
+                              {variantOption.map((items, index) => {
+                                 {
+                                    if (items.toLowerCase().includes('color')) {
+                                       return (<span key={index} className="d-flex my-2 align-items-baseline">
+                                          <Form.Label className="me-2 text-capitalize" style={{ width: '40%' }}>{items}:</Form.Label>
+                                          <Select
+                                             name="sub categories"
+                                             menuPortalTarget={document ? document.body : ''}
+                                             menuPosition="fixed"
+                                             menuPlacement="bottom"
+                                             className="categoryName me-0"
+                                             value={{ value: VariantTab[items] || '', label: VariantTab[items] || '' }}
+                                             onChange={(e) =>
+                                                setVariantTab((prev) => ({ ...prev, [items]: e.value }))
+                                             }
+                                             styles={{ ...customStyle, width: '100%' }}
+                                             options={ColorsOption.map(option => ({ value: option, label: option }))}
+                                          />
+                                       </span>
+                                       )
+                                    } else {
+                                       return (
+                                          <div key={index} className="d-flex my-2 align-items-baseline">
+                                             <Form.Label className="me-2 text-capitalize" style={{ width: '40%' }}>{items}:</Form.Label>
+                                             <Form.Control name={items} value={VariantTab[items] || ''} type={items.toLowerCase().includes('number') ? 'number' : 'text'} onChange={(e) =>
+                                                setVariantTab((prev) => ({ ...prev, [items]: e.target.value }))
+                                             } />
+                                          </div>
+                                       )
+                                    }
+                                 }
+                              })}
+                           </div>
+                        </Col>
+                        <Col md={2} className="d-flex align-items-center">
+                           <div ><Button className="add-Variant" onClick={(e)=>handleVariantTab2(e)} style={{ background: '#362465', border: 'none' }}>Add Variant</Button></div>
+                        </Col>
+                        <Col md={6} className="d-flex align-items-center">
+                           <div >{VariantTab2.map((items, index) => (<div key={index} className="border border-2 mx-2 px-2 py-1 rounded-4">{items}<IoMdCloseCircleOutline className="ms-1" onClick={(e) => handleDeleteVariantTab(index)} style={{ cursor: 'pointer' }} /></div>))}</div>
+                        </Col>
+                     </>)}
+               </Row>
+            </div>
          )}
       </>
 
