a0e688ace01655ca163938effbdc7e5d82b39522
avez07 <ansariaavez12345@gmail.com>
2024-07-07 20:33:36 +0530

    rate calculator

--- src/app/vender/products/add-product/[id]/page.js
+++ src/app/vender/products/add-product/[id]/page.js
@@ -22,6 +22,9 @@ const AddInfo = ({ params }) => {
    const [VariantTab, setVariantTab] = useState({})
    const { nightmode } = useContext(AuthContext)
    const [pointsCount, setPointsCount] = useState(1)
+   const [createTableButon, setCreateTableButton] = useState(false)
+   
+   const [TableData,setTableData] = useState()
 
    const customStyle = {
       control: (style) => ({ ...style, background: nightmode ? '#0c1220' : null, border: nightmode ? 'currentColor' : '' }),
@@ -63,21 +66,51 @@ const AddInfo = ({ params }) => {
       }))
 
    }
-   const handleVariantTab2 = (e)=>{
-     const data =  Object.values(VariantTab).join('/')
-     setVariantTab2((prev)=>([...prev,data]))
+   const handleVariantTab2 = (e) => {
+      const data = Object.values(VariantTab)
+      const key = Object.keys(VariantTab).filter((key) => !VariantTab[key])
+      if (key.length > 0) return alert(`${key.join(',')} feild is important`)
+      const same_array = VariantTab2.some((str) => data.every((value) => str.toLowerCase().includes(value.toLowerCase())))
+      const capitalizeFirstLetter = str => str.charAt(0).toUpperCase() + str.slice(1);
+      const capitalizedArray = data.map(capitalizeFirstLetter);
+      if (!same_array) setVariantTab2((prev) => ([...prev, capitalizedArray.join('/')]))
+      if (!same_array) Object.keys(VariantTab).forEach((key) => VariantTab[key] = '')
+      if (same_array) alert('Variant Already Present')
    }
-   const handleDeleteVariantTab = (index)=>{
-      const data = VariantTab2.splice(index,1)
-      setVariantTab2(data)
-}
-   console.log(VariantTab2)
+   const handleDeleteVariantTab = (index) => {
+      const newData = [...VariantTab2];
+      newData.splice(index, 1)
+      setVariantTab2(newData)
+   }
+   const handleTabData = (e,index)=>{
+      const {name,value} = e.target
+     const newData = [...TableData]
+     newData[index][name] = value
+     setTableData(newData);
+     calculateDerivedValues(index);
+   }
+   const calculateDerivedValues = (index) => {
+      const item = TableData[index];
+      let { cost, price ,gst,discount} = item;
+      if (cost && price) {
+         const gstRate = parseInt(gst)/100;
+         const CalPrice = parseInt(price) - parseInt(discount)
+         const margin = (parseFloat(CalPrice) - parseFloat(cost));
+         price = CalPrice
+        const finalPrice = parseFloat(price) + (parseFloat(price) * gstRate);
+        const newData = [...TableData];
+        newData[index] = { ...item,  finalPrice ,margin,price};
+        setTableData(newData);
+      }
+    };
+   console.log(TableData)
    useEffect(() => {
       setVariantTab(variantOption.reduce((acc, item) => {
          acc[item] = '';
          return acc;
       }, {}));
       setVariantTab2([])
+      setCreateTableButton(false)
    }, [variantOption]);
    useEffect(() => {
       const id = params.id
@@ -88,6 +121,18 @@ const AddInfo = ({ params }) => {
          alert(response.message)
       })
    }, [])
+   useEffect(()=>{
+      const initialData = VariantTab2.map(item => ({
+         quantity: 0,
+         cost: 0,
+         price: 0,
+         discount: 0,
+         gst: 12, // Assuming a fixed GST of 12%
+         margin: 0, // Placeholder for margin calculation
+         finalPrice: 0
+       }));
+       setTableData(initialData)
+   },[VariantTab2])
    // console.log(CategoryInput)
    return (
       <>
@@ -135,7 +180,7 @@ const AddInfo = ({ params }) => {
                                  menuPosition="fixed"
                                  menuPlacement="bottom"
                                  className="categoryName"
-                                 value={{ value: CategoryInput.details?.virtualInfo[item.name], label: CategoryInput.details?.virtualInfo[item.name] } || ''}
+                                 value={{ value: CategoryInput.details?.virtualInfo[item.name] || '', label: CategoryInput.details?.virtualInfo[item.name] || '' }}
                                  onChange={(e) =>
                                     setCategoryInput((prev) => ({
                                        ...prev,
@@ -154,7 +199,7 @@ const AddInfo = ({ params }) => {
                            </Col>
                         );
                      }
-                     return null; // Handle other types or conditions if necessary
+                     return null;
                   })
                ) : null}
             </Row>
@@ -191,7 +236,7 @@ const AddInfo = ({ params }) => {
                                  menuPosition="fixed"
                                  menuPlacement="bottom"
                                  className="categoryName"
-                                 value={{ value: CategoryInput.details?.virtualInfo[item.name], label: CategoryInput.details?.virtualInfo[item.name] } || ''}
+                                 value={{ value: CategoryInput.details?.virtualInfo[item.name] || '', label: CategoryInput.details?.virtualInfo[item.name] || '' }}
                                  onChange={(e) =>
                                     setCategoryInput((prev) => ({
                                        ...prev,
@@ -210,7 +255,7 @@ const AddInfo = ({ params }) => {
                            </Col>
                         );
                      }
-                     return null; // Handle other types or conditions if necessary
+                     return null;
                   })
                ) : null}
             </Row>
@@ -262,8 +307,8 @@ const AddInfo = ({ params }) => {
             <div>
                <Row className="g-3">
                   <Col md={12}>
-                     <div className="d-flex justify-content-around my-4"><span className="fw-semibold">Variant Containt:</span><span className="d-flex justify-content-around w-50">{CategoryData.details.VariantOption.map((items, index) => (
-                        <span><Form.Check type="checkbox" name={items} onChange={(e) => handleVariantOptionChange(e, index)} key={index} label={items} /></span>))}</span></div>
+                     <div className="d-flex justify-content-around my-4"><span className="fw-semibold">Variant Containt:</span><span className="d-flex justify-content-around w-50">{CategoryData.details?.VariantOption.map((items, index) => (
+                        <span key={`check-${index}`}><Form.Check type="checkbox" name={items} onChange={(e) => handleVariantOptionChange(e, index)} key={index} label={items} /></span>))}</span></div>
                   </Col>
                   {variantOption.length > 0 && (
                      <>
@@ -304,12 +349,58 @@ const AddInfo = ({ params }) => {
                            </div>
                         </Col>
                         <Col md={2} className="d-flex align-items-center">
-                           <div ><Button className="add-Variant" onClick={(e)=>handleVariantTab2(e)} style={{ background: '#362465', border: 'none' }}>Add Variant</Button></div>
+                           <div ><Button className="add-Variant" onClick={(e) => handleVariantTab2(e)} style={{ background: '#362465', border: 'none' }}>Add Variant</Button></div>
                         </Col>
                         <Col md={6} className="d-flex align-items-center">
-                           <div >{VariantTab2.map((items, index) => (<div key={index} className="border border-2 mx-2 px-2 py-1 rounded-4">{items}<IoMdCloseCircleOutline className="ms-1" onClick={(e) => handleDeleteVariantTab(index)} style={{ cursor: 'pointer' }} /></div>))}</div>
+                           <div className="d-flex flex-wrap" >{VariantTab2.map((items, index) => (<div key={index} className="border border-2 mx-2 my-1 d-flex align-items-center justify-content-between px-2 py-1 rounded-2">{items}<IoMdCloseCircleOutline className="ms-2 fs-5" onClick={(e) => handleDeleteVariantTab(index)} style={{ cursor: 'pointer' }} /></div>))}</div>
                         </Col>
+                        {VariantTab2.length > 0 && (
+                           <Col md={12}>
+                              <div ><Button className="add-Variant" onClick={(e) => setCreateTableButton(true)} style={{ background: '#362465', border: 'none' }}>Create Table</Button></div>
+                           </Col>
+                        )}
                      </>)}
+                  {createTableButon && (
+                     <Col md={12} style={{ overflowX: 'scroll' }}>
+                        <table style={{ width: '100%', minWidth: '100vw' }} className="orderTable variantCategoryTable">
+                           <thead style={{ height: '65px' }}>
+                              <tr>
+                                 <th>Category Name</th>
+
+                                 {CategoryData.details.VariantData.map((items) => (
+                                    <th key={items.name + '1'}>{items.name}</th>
+                                 ))}
+                                 <th>Quantity</th>
+                                 <th>Cost</th>
+                                 <th>Price</th>
+                                 <th>Discount(Rs)</th>
+                                 <th>GST</th>
+                                 <th>Margin</th>
+                                 <th>Aves Price</th>
+
+                              </tr>
+                           </thead>
+                           <tbody>
+                              {VariantTab2.map((items, index) => (
+                                 <tr className="text-center" key={`row-${index}`}>
+                                    <td>{items}</td>
+                                    {CategoryData.details.VariantData.map((items) => (
+                                       <td key={`datas-${items.name}`}><Form.Control  type="text" /></td>
+                                    ))}
+                                    <td><Form.Control type="text" /></td>
+                                    <td><Form.Control name="cost" onChange={(e)=>handleTabData(e,index)} type="text" /></td>
+                                    <td><Form.Control name="price" onChange={(e)=>handleTabData(e,index)} type="text" /></td>
+                                    <td><Form.Control name="discount" onChange={(e)=>handleTabData(e,index)} type="text" /></td>
+                                    <td name="gst">{TableData[index]?.gst || 0}%</td>
+                                    <td name="margin">{TableData[index]?.margin || 0}</td>
+                                    <td name='avesPrice'>{TableData[index]?.finalPrice || 0}</td>
+
+                                 </tr>
+                              ))}
+                           </tbody>
+                        </table>
+                     </Col>
+                  )}
                </Row>
             </div>
          )}
